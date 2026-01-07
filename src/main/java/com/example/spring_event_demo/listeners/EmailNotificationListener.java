78f5ad328d9c63ec71bad2742ffccb087106ae3e
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-07 09:49:42 +0530

    Updated

--- src/main/java/com/example/spring_event_demo/listeners/EmailNotificationListener.java
+++ src/main/java/com/example/spring_event_demo/listeners/EmailNotificationListener.java
@@ -2,12 +2,14 @@ package com.example.spring_event_demo.listeners;
 
 import com.example.spring_event_demo.events.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailNotificationListener {
 
     @EventListener
+    @Order(2)
     public void handleOrderCreatedAndSendEmail(OrderCreatedEvent event){
         System.out.println("Sending email for order "+event.getOrderId());
     }
--- src/main/java/com/example/spring_event_demo/listeners/LogOrderDetailsListener.java
+++ src/main/java/com/example/spring_event_demo/listeners/LogOrderDetailsListener.java
@@ -2,12 +2,14 @@ package com.example.spring_event_demo.listeners;
 
 import com.example.spring_event_demo.events.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LogOrderDetailsListener {
 
     @EventListener
+    @Order(1)
     public void logOrderCreated(OrderCreatedEvent event){
         System.out.println("Order placed for orderId "+event.getOrderId());
     }
